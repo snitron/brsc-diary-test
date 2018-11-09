ceacec4e68a8887c70e06db0938b87feee436351
snitron <nvar2@yandex.ru>
2018-11-09 22:04:25 +0300

    test

--- web/getIdFromSession.php
+++ web/getIdFromSession.php
@@ -1,8 +1,8 @@
 <?php
-session_name(filter_input(INPUT_GET, "name", FILTER_SANITIZE_STRING));
+session_name(filter_input(INPUT_GET, "name", FILTER_SANITIZE_STRING) . "");
 session_start();
 
-$id = filter_input(INPUT_GET, "id", FILTER_SANITIZE_STRING);
+$id = filter_input(INPUT_GET, "id", FILTER_SANITIZE_STRING) . "";
 
 echo $id;
 echo $_SESSION[$id];
