2b2bb6338505687a5d8433853a6365e0f21a2d37
snitron <nvar2@yandex.ru>
2018-11-10 20:46:40 +0300

    test

--- web/getIdNew.php
+++ web/getIdNew.php
@@ -14,9 +14,12 @@ $post_array['Login'] = $login;
 $post_array['Password'] = $password;
 
 $snoopy->maxredirs = 2;
+$snoopy->cookies = array("PHPSESSID=". session_id());
+$snoopy->setcookies();
 $snoopy->submit("https://edu.brsc.ru/Logon/Index", $post_array);
 $snoopy->results;
 $snoopy->cookies = array("PHPSESSID=". session_id());
+$snoopy->setcookies();
 
 $snoopy->submit("https://edu.brsc.ru/privateoffice", $post_array);
 
