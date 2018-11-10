daa5029951f0ba775ea82a7f4b9cd01bc4930795
snitron <nvar2@yandex.ru>
2018-11-10 18:01:42 +0300

    test

--- web/getIdFromSession.php
+++ web/getIdFromSession.php
@@ -1,41 +1,14 @@
 <?php
-$s = getSessionData();
 
-echo $s['123456']['login'];
-echo $s['123456']['password'];
+ini_set('display_startup_errors', 1);
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
 
+session_start();
 
-function getSessionData ($session_name = 'PHPSESSID', $session_save_handler = 'files') {
-    $session_data = array();
-    # did we get told what the old session id was? we can't continue it without that info
-    if (array_key_exists($session_name, $_COOKIE)) {
-        # save current session id
-        $session_id = $_COOKIE[$session_name];
-        $old_session_id = session_id();
+$id = filter_input(INPUT_GET, "id", FILTER_SANITIZE_STRING) . "";
 
-        # write and close current session
-        session_write_close();
+$s = $_SESSION[$id];
 
-        # grab old save handler, and switch to files
-        $old_session_save_handler = ini_get('session.save_handler');
-        ini_set('session.save_handler', $session_save_handler);
-
-        # now we can switch the session over, capturing the old session name
-        $old_session_name = session_name($session_name);
-        session_id($session_id);
-        session_start();
-
-        # get the desired session data
-        $session_data = $_SESSION;
-
-        # close this session, switch back to the original handler, then restart the old session
-        session_write_close();
-        ini_set('session.save_handler', $old_session_save_handler);
-        session_name($old_session_name);
-        session_id($old_session_id);
-        session_start();
-    }
-
-    # now return the data we just retrieved
-    return $session_data;
-}
+echo $s['login'];
+echo $s['password'];
--- web/getIdNew.php
+++ web/getIdNew.php
@@ -1,60 +1,36 @@
 <?php
-require __DIR__ . "/../vendor/autoload.php";
 
-use Snoopy\Snoopy;
-use \DiDom\Document;
-use Behat\Mink\Session;
-use Behat\Mink\Driver\GoutteDriver;
+ini_set('display_startup_errors', 1);
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
 
-    $login = filter_input(INPUT_GET, "login", FILTER_SANITIZE_STRING);
-    $password = filter_input(INPUT_GET, "password", FILTER_SANITIZE_STRING);
 
-   /* $snoopy = new Snoopy();
+$login = filter_input(INPUT_GET, "login", FILTER_SANITIZE_STRING);
+$password = filter_input(INPUT_GET, "password", FILTER_SANITIZE_STRING);
 
-    $post_array = array();
-    $post_array['Login'] = $login;
-    $post_array['Password'] = $password;
-
-    $snoopy->maxredirs = 2;
-    $snoopy->submit("https://edu.brsc.ru/Logon/Index", $post_array);
-    $snoopy->results;
-
-    $snoopy->submit("https://edu.brsc.ru/User/Diary");
-    $html = new Document($snoopy->results);
-
-    $id = parseId($html->find("a.h5")[0]->getAttribute("href"));*/
 $id = '123456';
 
-    session_start();
+session_start();
 
+echo $id;
 
-    if($id == ""){
-        echo "";
-        session_register_shutdown();
-    }else{
-        echo $id;
+$user = array();
 
-        $user = array();
+$user['login'] = $login;
+$user['password'] = $password;
+$user['child_ids'] = null;
 
-        $user['login'] = $login;
-        $user['password'] = $password;
-        $user['child_ids'] = null;
+$_SESSION[$id] = $user;
+session_commit();
 
-        $_SESSION[$id] = $user;
-        session_commit();
-    }
-
-
-
-
-
+/*
 function parseId($string)
 {
-    $b = stristr($string, "?");
-    $c = substr($b, 1);
+$b = stristr($string, "?");
+$c = substr($b, 1);
 
-    $output = array();
-    parse_str($c, $output);
+$output = array();
+parse_str($c, $output);
 
-    return $output['UserId'];
-}
+return $output['UserId'];
+}*/
