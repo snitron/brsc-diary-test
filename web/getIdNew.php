a4ce04d4e8478c0634d76faa6f613d156f662e55
snitron <nvar2@yandex.ru>
2018-11-10 20:45:02 +0300

    test

--- web/getIdNew.php
+++ web/getIdNew.php
@@ -1,19 +1,27 @@
 <?php
-
-ini_set('display_startup_errors', 1);
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
+require __DIR__ . "/../vendor/autoload.php";
+use Snoopy\Snoopy;
 
 
 $login = filter_input(INPUT_GET, "login", FILTER_SANITIZE_STRING);
 $password = filter_input(INPUT_GET, "password", FILTER_SANITIZE_STRING);
 
-$id = "test123456";
-
 session_start();
+$snoopy = new Snoopy();
 
-echo $id;
+$post_array = array();
+$post_array['Login'] = $login;
+$post_array['Password'] = $password;
 
+$snoopy->maxredirs = 2;
+$snoopy->submit("https://edu.brsc.ru/Logon/Index", $post_array);
+$snoopy->results;
+$snoopy->cookies = array("PHPSESSID=". session_id());
+
+$snoopy->submit("https://edu.brsc.ru/privateoffice", $post_array);
+
+echo $snoopy->results;
+/*
 $user = array();
 
 $user['login'] = $login;
@@ -22,7 +30,7 @@ $user['child_ids'] = null;
 
 $_SESSION[$id] = $user;
 session_commit();
-
+*/
 /*
 function parseId($string)
 {
