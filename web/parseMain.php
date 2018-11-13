a34c06d938c867a9cfd44c8d0d5e3acebd823059
snitron <nvar2@yandex.ru>
2018-11-13 22:08:03 +0300

    test

--- web/parseMain.php
+++ web/parseMain.php
@@ -24,10 +24,10 @@ class DaySheldule
 
 $headers = getallheaders();
 //if ($headers['User-Agent'] == 'Nitron Apps BRSC Diary Http Connector') {
-    $action = filter_input(INPUT_POST, "action", FILTER_SANITIZE_STRING);
+    $action = filter_input(INPUT_GET, "action", FILTER_SANITIZE_STRING);
 
-    $login = filter_input(INPUT_POST, 'login', FILTER_SANITIZE_STRING);
-    $password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING);
+    $login = filter_input(INPUT_GET, 'login', FILTER_SANITIZE_STRING);
+    $password = filter_input(INPUT_GET, 'password', FILTER_SANITIZE_STRING);
 
     $snoopy = new Snoopy();
 
