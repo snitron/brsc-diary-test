bd807223b0f29fc390a2fff40bc314dcd3ad8a43
snitron <nvar2@yandex.ru>
2018-11-15 14:56:03 +0300

    test

--- web/getName.php
+++ web/getName.php
@@ -4,10 +4,14 @@ require __DIR__ . "/../vendor/autoload.php";
 use Snoopy\Snoopy;
 use DiDom\Document;
 
-class Person
-{
+class PersonOld {
     public $name = "";
     public $img = "";
+}
+
+class Person {
+    public $child_names = array();
+    public $name = "";
 }
 
 $headers = getallheaders();
@@ -29,34 +33,34 @@ if ($headers['User-Agent'] == 'Nitron Apps BRSC Diary Http Connector') {
 
     if ($version != null) {
         $option = filter_input(INPUT_GET, "option", FILTER_SANITIZE_STRING);
+        $person = new Person();
 
         if ($option == "one") {
+            $person->child_names = null;
             $userID = filter_input(INPUT_GET, "child_ids", FILTER_SANITIZE_STRING);
             $snoopy->submit("https://edu.brsc.ru/user/diary/diaryresult?UserId=" . $userID);
             $html = new Document($snoopy->results);
 
-           $name = parseName($html->find("tr")[0]->find("th")[0]->text());
+            $person->name = trim(parseName($html->find("tr")[0]->find("th")[0]->text()));
 
-           echo trim($name);
+           echo json_encode($person);
         } else {
             $child_ids = json_decode(filter_input(INPUT_GET, "child_ids", FILTER_SANITIZE_STRING));
-
-            $names = array();
 
             for ($i = 0; $i < count($child_ids); $i++) {
                 $snoopy->submit("https://edu.brsc.ru/user/diary/diaryresult?UserId=" . $userID);
                 $html = new Document($snoopy->results);
 
-                $names = parseName($html->find("tr")[0]->find("th")[0]->text());
+                $person->child_names[$i] = trim(parseName($html->find("tr")[0]->find("th")[0]->text()));
             }
 
-            echo json_encode($names);
+            echo json_encode($person);
         }
     } else {
         $userID = filter_input(INPUT_GET, "child_ids", FILTER_SANITIZE_STRING);
         $snoopy->submit("https://edu.brsc.ru/user/diary/diaryresult?UserId=" . $userID);
         $html = new Document($snoopy->results);
-        $result = new Person();
+        $result = new PersonOld();
         $result->name = trim(parseName($html->find("tr")[0]->find("th")[0]->text()));
         $result->img = $html->find("span.pull-left")[1]->first("img")->attr("src");
         echo json_encode($result);
